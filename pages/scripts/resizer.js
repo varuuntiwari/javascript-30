72c4c007d9bba57027ed2c8d615a28b51f84e6b6
Varun Tiwari <varuntiwari2001@gmail.com>
2022-02-16 00:52:07 +0530

    Added listener for mobile devices

--- pages/scripts/resizer.js
+++ pages/scripts/resizer.js
@@ -1,21 +1,24 @@
 window.onload = () => {
-    document.querySelector('#color').addEventListener('change', () => {
+    function changeBG() {
         let color = document.querySelector('#color').value;
         console.log(color);
         let r = document.querySelector(':root');
         r.style.setProperty('--bg-color', color);
-    });
-
-    document.querySelector('#blur').addEventListener('mousemove', () => {
+    }
+    function changeBlur() {
         let blur = document.querySelector('#blur').value;
         document.querySelector('#blurval').innerText = blur;
         let r = document.querySelector(':root');
         r.style.setProperty('--blur', blur+"px");
-    });
-
-    document.querySelector('#spacing').addEventListener('mousemove', () => {
+    }
+    function changeSpacing() {
         let spacing = document.querySelector('#spacing').value;
         let r = document.querySelector(':root');
         r.style.setProperty('--spacing', spacing+"px");
-    });
+    }
+    document.querySelector('#color').addEventListener('change', changeBG);
+    document.querySelector('#blur').addEventListener('mousemove', changeBlur);
+    document.querySelector('#blur').addEventListener('change', changeBlur);
+    document.querySelector('#spacing').addEventListener('mousemove', changeSpacing);
+    document.querySelector('#spacing').addEventListener('change', changeSpacing);
 }
